8a010f7c44bd155c7e15fb1fdd9a5200e773c278
Arie Fisher <jbgirl0@gmail.com>
2025-05-14 16:16:03 -0400

    added some menus to code

--- CarDealership/src/main/java/com/pluralsight/Dealership.java
+++ CarDealership/src/main/java/com/pluralsight/Dealership.java
@@ -16,62 +16,77 @@ public class Dealership {
     }
 
     public String getName() {
+
         return name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public String getAddress() {
+
         return address;
     }
 
     public void setAddress(String address) {
+
         this.address = address;
     }
 
     public String getPhoneNumber() {
+
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
+
         this.phoneNumber = phoneNumber;
     }
 
     public ArrayList<Vehicle> getVehiclesByPrice(double min, double max){
+
         return null;
     }
 
     public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model){
+
         return null;
     }
 
     public ArrayList<Vehicle> getVehiclesByYear(int min, int max){
+
         return null;
     }
 
     public ArrayList<Vehicle> getVehiclesByColor(String color){
+
         return null;
     }
 
     public ArrayList<Vehicle> getVehiclesByMilage(int min, int max){
+
         return null;
     }
 
     public ArrayList<Vehicle> getVehiclesByType(String type){
+
         return null;
     }
 
     public ArrayList<Vehicle> getAllVehicles(){
+
         return null;
     }
 
     public void addVehicle(Vehicle vehicle){
+
         inventory.add(vehicle);
     }
 
     public void removeVehicle(Vehicle vehicle){
+
         inventory.remove(vehicle);
     }
 
--- CarDealership/src/main/java/com/pluralsight/UserInterface.java
+++ CarDealership/src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,9 @@
 package com.pluralsight;
 
+
+import java.util.List;
+import java.util.Scanner;
+
 public class UserInterface {
     Dealership dealership;
 
@@ -7,5 +11,86 @@ public class UserInterface {
         DealershipFileManager fileManager = new DealershipFileManager();
         dealership = fileManager.getDealership();
         System.out.println(dealership);
+    }
+    Scanner scantron = new Scanner(System.in);
+
+    boolean running = true;
+    while (running) {
+        System.out.println("Welcome! Please choose your option below: ");
+        String choice = scantron.nextLine();
+
+        switch (choice) {
+            case "1":
+                proccessGetByPrice();
+                break;
+
+            case "2":
+                proccessGetByMakeModel();
+                break;
+
+            case "3":
+                proccessGetByYear();
+                break;
+
+            case "4":
+                processGetByColor();
+                break;
+
+            case "5":
+                processGetByMileage();
+                break;
+
+            case "6":
+                processGetByVehicleType();
+                break;
+
+            case "7":
+                processGetByAllVehicles();
+                break;
+
+            case "8":
+                processRemoveVehicleRequest();
+                break;
+
+            case "9":
+                System.out.println("Exiting Program...");
+                running = false;
+                break;
+
+            default:
+                System.out.println("Invalid inquiry try again");
+        }
+    }
+
+    scantron.close();
+    }
+
+    private void displayMenu() {
+        System.out.println("\n--- Dealership Menu ---");
+        System.out.println("1. View All Vehicles");
+        System.out.println("2. Search by Price");
+        System.out.println("3. Search by Make and Model");
+        System.out.println("4. Search by Year");
+        System.out.println("5. Search by Color");
+        System.out.println("6. Search by Vehicle Type");
+        System.out.println("7. Add a Vehicle");
+        System.out.println("8. Remove a Vehicle");
+        System.out.println("9. Exit");
+
+    }
+public void proccessGetByPrice() {
+    Scanner scantron = new Scanner(System.in);
+    try {
+        System.out.println("Enter minimum price: ");
+        double min = Double.parseDouble(scantron.nextLine);
+
+        System.out.println("Enter maximum price: ");
+        double max = Double.parseDouble(scantron.nextLine);
+
+        List<Vehicle> results = proccessGetByPrice(min, max);
+        displayVehicles(results);
+    }
+    catch (NumberFormatException e) {
+        System.out.println("Invalid selection.");
     }
 }
